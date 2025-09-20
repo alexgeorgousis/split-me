c21db83977f74e1e144afd6d69506a2a8f09537d
Alex Georgousis <alex.georgousis@hey.com>
2025-09-20 08:53:21 +0100

    Enable attaching a receipt to Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -65,6 +65,6 @@ class OrdersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def order_params
-      params.expect(order: [ :total ])
+      params.expect(order: [ :total, :receipt ])
     end
 end
