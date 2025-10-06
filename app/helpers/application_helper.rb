f4dd8ae83401a4749370de8cdb4579d6d90cd305
Alex Georgousis <alex.georgousis@hey.com>
2025-10-06 15:05:48 +0100

    Extract SVG icons to reusable files (#12)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def icon(name, css_class: nil)
+    file_path = Rails.root.join("app", "assets", "images", "#{name}.svg")
+    return unless File.exist?(file_path)
+
+    svg = File.read(file_path)
+
+    if css_class
+      svg = svg.sub(/<svg/, "<svg class=\"#{css_class}\"")
+    end
+
+    svg.html_safe
+  end
 end
