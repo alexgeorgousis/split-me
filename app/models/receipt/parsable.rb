898b0e2e9f0ee07aec82905000b867a7271a3de7
Alex Georgousis <alex.georgousis@hey.com>
2025-10-09 17:25:06 +0100

    Minor: Improve error message

--- app/models/receipt/parsable.rb
+++ app/models/receipt/parsable.rb
@@ -12,7 +12,7 @@ module Receipt::Parsable
     begin
       file.blob.open do |pdf_file|
         reader = PDF::Reader.new(pdf_file)
-        raise "PDF has no pages" if reader.page_count == 0
+        raise "Receipt file PDF has no pages" if reader.page_count == 0
 
         pages_to_process = [ reader.page_count, 3 ].min
         text_content = reader.pages.first(pages_to_process).map.with_index do |page, index|
