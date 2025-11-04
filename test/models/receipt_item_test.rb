aa59f04b5f1e74be317f16fb79aa1a14a7014a2d
Alex Georgousis <alex.georgousis@hey.com>
2025-11-04 09:02:59 +0000

    Minor test refactor

--- test/models/receipt_item_test.rb
+++ test/models/receipt_item_test.rb
@@ -7,15 +7,18 @@ class ReceiptItemTest < ActiveSupport::TestCase
     @undecided = receipt_items(:undecided)
   end
 
-  test "calculates my share correctly" do
+  test "calculating shares" do
     assert_equal 10, @mine.my_share
     assert_equal 10, @shared.my_share
     assert_equal 0, @undecided.my_share
-  end
 
-  test "calculates their share correctly" do
     assert_equal 0, @mine.their_share
     assert_equal 10, @shared.their_share
     assert_equal 30, @undecided.their_share
+  end
+
+  test "creating a receipt item that matches a favourite will automatically mark it as mine" do
+    item = ReceiptItem.create! receipt: receipts(:one), name: "one", price: 15
+    assert item.mine?
   end
 end
--- test/models/receipt_test.rb
+++ test/models/receipt_test.rb
@@ -5,15 +5,9 @@ class ReceiptTest < ActiveSupport::TestCase
     @receipt = receipts(:one)
   end
 
-  test "calculates total correctly" do
+  test "calculating totals" do
     assert_equal 60, @receipt.total
-  end
-
-  test "calculates my total correctly" do
     assert_equal 20, @receipt.my_total
-  end
-
-  test "calculates their total correctly" do
     assert_equal 40, @receipt.their_total
   end
 end
